e5865d2b68f05b32c81f2e072ea6c3994813432f
Victor Salazar Lopez <salazarlopezvictor@outlook.com>
2026-04-01 02:14:44 -0600

    feat: add basic footer with GitHub link

--- src/routes/(public)/+page.svelte
+++ src/routes/(public)/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import EventInformationSection from './_components/event-information.svelte'
+  import Footer from './_components/footer.svelte'
   import Hero from './_components/hero.svelte'
   import StagesSection from './_components/stages.svelte'
 </script>
@@ -7,3 +8,4 @@
 <Hero />
 <EventInformationSection />
 <StagesSection />
+<Footer />
