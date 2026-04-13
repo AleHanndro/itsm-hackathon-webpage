c09adcff74240368f03d43ab567e3f872a5c6272
Victor Salazar Lopez <salazarlopezvictor@outlook.com>
2026-04-13 17:03:40 -0600

    feat: move prerender to route layout
    Move prerender to the root layout so all public routes are prerendered
    during build.

--- src/routes/(public)/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import type { PageServerLoad } from './$types'
-
-export const prerender = true
-
-// eslint-disable-next-line @typescript-eslint/require-await -- Just a placeholder for now
-export const load: PageServerLoad = async () => {
-  return {}
-}
