170dda6048531a5405b10d4e814dc2ea536929da
Victor Salazar Lopez <salazarlopezvictor@outlook.com>
2026-05-10 22:29:18 -0600

    feat(auth): implement authorization handling for dashboard routes

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,11 @@
 import '$lib/z'
-import type { Handle } from '@sveltejs/kit'
+import type { Pathname } from '$app/types'
 
 import { building } from '$app/environment'
 import { auth } from '$lib/server/auth'
+import { hasAnyRole, hasRole } from '$lib/server/utils'
+import { type Handle, redirect } from '@sveltejs/kit'
+import { sequence } from '@sveltejs/kit/hooks'
 import { svelteKitHandler } from 'better-auth/svelte-kit'
 
 const handleBetterAuth: Handle = async ({ event, resolve }) => {
@@ -14,4 +17,24 @@ const handleBetterAuth: Handle = async ({ event, resolve }) => {
   return svelteKitHandler({ auth, building, event, resolve })
 }
 
-export const handle: Handle = handleBetterAuth
+const handleAuthorization: Handle = ({ event, resolve }) => {
+  const routeId = event.route.id
+  const user = event.locals.user
+
+  if (event.url.pathname.includes('/dashboard') && !user)
+    return redirect(303, '/login' satisfies Pathname)
+
+  if (routeId?.includes('/(admin-only)') && !hasRole(user?.role, 'admin'))
+    return redirect(303, '/dashboard/evento' satisfies Pathname)
+
+  if (routeId?.includes('/(staff)') && !hasAnyRole(user?.role, ['admin', 'staff']))
+    return redirect(303, '/dashboard' satisfies Pathname)
+
+  // admin or staff users cannot access user routes
+  if (routeId?.includes('/(user)') && hasAnyRole(user?.role, ['admin', 'staff']))
+    return redirect(303, '/dashboard/evento' satisfies Pathname)
+
+  return resolve(event)
+}
+
+export const handle: Handle = sequence(handleBetterAuth, handleAuthorization)
