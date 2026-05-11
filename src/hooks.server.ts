28609c30ebb1bf95baa0a82eb404f02d28df8869
Victor Salazar Lopez <salazarlopezvictor@outlook.com>
2026-05-10 19:49:59 -0600

    feat(auth): implement better authentication handling in hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,1 +1,17 @@
 import '$lib/z'
+import type { Handle } from '@sveltejs/kit'
+
+import { building } from '$app/environment'
+import { auth } from '$lib/server/auth'
+import { svelteKitHandler } from 'better-auth/svelte-kit'
+
+const handleBetterAuth: Handle = async ({ event, resolve }) => {
+  const session = await auth.api.getSession({ headers: event.request.headers })
+
+  event.locals.session = session?.session ?? null
+  event.locals.user = session?.user ?? null
+
+  return svelteKitHandler({ auth, building, event, resolve })
+}
+
+export const handle: Handle = handleBetterAuth
