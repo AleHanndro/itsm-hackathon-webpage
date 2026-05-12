b603a3a58595fecb585e470a922ca6395fbd239b
Victor Salazar Lopez <salazarlopezvictor@outlook.com>
2026-05-12 16:44:36 -0600

    fix: createUserSchema does not automatically update roles enum

--- src/routes/(protected)/dashboard/evento/(admin-only)/usuarios/schema.ts
+++ src/routes/(protected)/dashboard/evento/(admin-only)/usuarios/schema.ts
@@ -7,7 +7,7 @@ export const createUserSchema = z.object({
   email: z.email('Correo electrónico inválido.'),
   name: z.string().min(2, 'El nombre debe tener al menos 2 caracteres.'),
   password: z.string().min(8, 'La contraseña debe tener al menos 8 caracteres.'),
-  role: z.enum(['user', 'staff', 'admin', 'evaluator'], {
+  role: z.enum(roleEnumValues, {
     error: 'Rol inválido.',
   }),
 })
