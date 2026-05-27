e317ddb9566f683de26e881a1ef6cc3e1967088f
Victor Salazar Lopez <salazarlopezvictor@outlook.com>
2026-05-27 17:34:35 -0600

    fix: change start date to may 28th

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -2,7 +2,7 @@ import type { Post } from '$content-collections'
 
 import { engineerings } from '$lib/schema/pre-registrations'
 
-export const EVENT_START_DATE = new Date('2026-05-29T09:00:00-06:00')
+export const EVENT_START_DATE = new Date('2026-05-28T14:00:00-06:00')
 
 export const SITE_TITLE = 'ITSM Hackatón'
 export const SITE_DESCRIPTION = 'ITSM Hackatón 2026'
